9beea676c06922a95a0a13ab78fee48ef6e1fb91
mumakau <mukiimakau@gmail.com>
2022-03-29 10:33:24 +0300

    ready for deployment

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ app.use(express.static(__dirname + '/dist/ang-on-heroku'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/ang-on-heroku/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
